e2f62a4ff844e135f6da29fa94c87cd634b60337
kacper.lechanski <kacperlechanski19@o2.pl>
2024-03-24 18:33:01 +0100

    Finish CheckModules function

--- PasswordReminder.ps1
+++ PasswordReminder.ps1
@@ -5,12 +5,13 @@ function SetPowerShell {
 }
 function CheckModules {
     try {
-        $moduleName = "MSOssnline"
+        $moduleName = "MSOnline"
         Write-Host "Checking required modules... Please wait.." -ForegroundColor Yellow
         Start-Sleep -Seconds 3
         Get-InstalledModule -Name $moduleName -ErrorAction Stop
         Write-Host "Module " $moduleName " is installed. Please wait for the module update.." -ForegroundColor Yellow
         Update-Module -Name $moduleName
+        Start-Sleep -Seconds 3
     }
     catch {
         Write-Output "Module $moduleName is not installed. Wait for the install.."
